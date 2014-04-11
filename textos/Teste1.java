4c720c2a32caef17bda955ba3e6fc64d1ec73b68
Ton Carvalho <root@localhost.localdomain>
2014-04-11 15:09:13 -0300

    alterado

--- textos/Teste1.java
+++ textos/Teste1.java
@@ -2,5 +2,7 @@ public void static main(String args[]){
 
 	System.out.println(" ola mundo");
 	System.out.println(" ola mundo novamente");
-System.out.println(" eita pega preciso lembrar os comandos do vi");
+        System.out.println(" eita pega preciso lembrar os comandos do vi");
+        System.out.println(" estilingou");
+
 }
