26f2ad640a6bde162fcc76afcaf6f6f5574e63ff
Ton Carvalho <root@localhost.localdomain>
2014-04-11 14:41:44 -0300

    alteracao

--- textos/Teste1.java
+++ textos/Teste1.java
@@ -2,5 +2,5 @@ public void static main(String args[]){
 
 	System.out.println(" ola mundo");
 	System.out.println(" ola mundo novamente");
-
+System.out.println(" eita pega preciso lembrar os comandos do vi");
 }
