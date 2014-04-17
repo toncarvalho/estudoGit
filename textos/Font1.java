cf42730dfe2f4b58c89f91b821f3a5ec138f40f9
ton <ton@localhost.localdomain>
2014-04-17 13:19:57 -0300

    dois arquivos de uma vez

--- textos/Font1.java
+++ textos/Font1.java
@@ -1,8 +1,11 @@
 public class Font1 {
-public static void main(String args[]){
 
-	System.out.println("vixi" ) ;
-	System.out.println("vixi de novo" ) ;
-	System.out.println("vixi de novo, novamente" ) ;
-}
+	public static void main(String args[]){
+	
+		System.out.println("vixi" ) ;
+		System.out.println("vixi de novo" ) ;
+		System.out.println("vixi de novo, novamente" ) ;
+		System.out.println("vixi de novo, de novo outra vez" ) ;
+
+	}
 }
--- textos/Teste1.java
+++ textos/Teste1.java
@@ -1,9 +1,12 @@
+public class Teste1{
 public void static main(String args[]){
 
-	System.out.println(" ola mundo");
-	System.out.println(" ola mundo novamente");
-        System.out.println(" eita pega preciso lembrar os comandos do vi");
-        System.out.println(" estilingou");
-        System.out.println(" tenso");
-
+		System.out.println(" ola mundo");
+		System.out.println(" ola mundo novamente");
+	        System.out.println(" eita pega preciso lembrar os comandos do vi");
+	        System.out.println(" estilingou");
+	        System.out.println(" tenso");
+		System.out.println(" encomenda");
+		System.out.println(" meu teste");
+	}
 }
