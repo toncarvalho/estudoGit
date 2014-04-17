77a7d50ef7c3f1d1c1c50b2dbb66b8d776b056d2
ton <ton@localhost.localdomain>
2014-04-17 13:13:21 -0300

    eita pega

--- textos/Teste1.java
+++ textos/Teste1.java
@@ -4,5 +4,6 @@ public void static main(String args[]){
 	System.out.println(" ola mundo novamente");
         System.out.println(" eita pega preciso lembrar os comandos do vi");
         System.out.println(" estilingou");
+        System.out.println(" tenso");
 
 }
